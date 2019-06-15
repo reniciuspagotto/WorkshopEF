40da467f5fa4ee1f1e9c10a141c0db6d93ab6e65
Renicius Pagotto <renicius.pagotto@outlook.com>
2019-06-14 21:20:16 -0300

    Ana Manzan é linda

---
+++ CadastroDeClientes.Domain/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace CadastroDeClientes.Domain
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Telefone { get; set; }
+        //public Endereco Endereco { get; set; }
+    }
+}
